7c39993d7c90ef7a64a7c7a6543b938e57648afe
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-20 01:56:36 +0530

    faculty backend app

---
+++ src/test/java/com/nestdigital/facultybackendapp/FacultyBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.facultybackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
